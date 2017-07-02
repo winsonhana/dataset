44a7bdeb3d3d97baf3f24b6fdfd632f57a819eb2
winsonhana <winson@hanalytics.sg>
2017-07-03 06:54:21 +0800

    deeper

--- WMH_model3D.py
+++ WMH_model3D.py
@@ -84,7 +84,7 @@ def model3D_2(img=(83, 256, 256)):
         seq.add(Conv3D_Tranpose1(input_channels=10, num_filters=3, output_shape=img, kernel_size=(5,5,5), stride=(2,2,2), padding='SAME'))
         ##        
         seq.add(RELU())        
-        seq.add(Conv3D(input_channels=3, num_filters=3, kernel_size=(5,5,5), stride=convStride, padding='SAME'))        
+        seq.add(Conv3D(input_channels=3, num_filters=3, kernel_size=(1,1,1), stride=convStride, padding='SAME'))        
         ##
         seq.add(Softmax())
         #seq.add(Sigmoid())
