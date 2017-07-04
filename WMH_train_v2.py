76bd93f5efa508574d97d6dcded295e09d8f97e5
winsonhana <winson@hanalytics.sg>
2017-07-04 09:12:44 +0800

    ggg

--- WMH_train_v2.py
+++ WMH_train_v2.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     saver = tf.train.Saver()
     
     
-    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.98)
+    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
     with tf.Session(config = tf.ConfigProto(gpu_options = gpu_options)) as sess:
         init = tf.global_variables_initializer()
         sess.run(init)
